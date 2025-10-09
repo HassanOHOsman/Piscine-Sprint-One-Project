5fd418473de7937f9e72d60a7c482be292170f28
sheida-shab <shabankary@gmail.com>
2025-10-09 11:13:44 +0100

    moved function calculateRevisionDate to module common.mjs  . added test for this function

--- common.mjs
+++ common.mjs
@@ -1,3 +1,28 @@
 export function getUserIds() {
   return ["1", "2", "3", "4", "5"];
 }
+
+export function calculateRevisionDate(selectedDate) {
+  const revisionDateArray = [];
+  //one week later
+  const oneWeek = new Date(selectedDate);
+  oneWeek.setDate(selectedDate.getDate() + 7);
+  revisionDateArray.push(oneWeek.toLocaleDateString());
+  //One Month Later
+  const oneMonth = new Date(selectedDate);
+  oneMonth.setMonth(selectedDate.getMonth() + 1);
+  revisionDateArray.push(oneMonth.toLocaleDateString());
+  //Three Month Later
+  const threeMonth = new Date(selectedDate);
+  threeMonth.setMonth(selectedDate.getMonth() + 3);
+  revisionDateArray.push(threeMonth.toLocaleDateString());
+  //Six Month Later
+  const sixMonth = new Date(selectedDate);
+  sixMonth.setMonth(selectedDate.getMonth() + 6);
+  revisionDateArray.push(sixMonth.toLocaleDateString());
+  //One Year
+  const oneYear = new Date(selectedDate);
+  oneYear.setFullYear(selectedDate.getFullYear() + 1);
+  revisionDateArray.push(oneYear.toLocaleDateString());
+  return revisionDateArray;
+}
--- common.test.mjs
+++ common.test.mjs
@@ -1,7 +1,21 @@
 import { getUserIds } from "./common.mjs";
+import { calculateRevisionDate } from "./common.mjs";
 import assert from "node:assert";
 import test from "node:test";
 
 test("User count is correct", () => {
   assert.equal(getUserIds().length, 5);
+});
+
+test("calculateRevisionDate returns correct future dates", () => {
+  const selectedDate = new Date("2026-07-01");
+  const expectedDates = [
+    new Date("2026-07-08").toLocaleDateString(),
+    new Date("2026-08-01").toLocaleDateString(),
+    new Date("2026-10-01").toLocaleDateString(),
+    new Date("2027-01-01").toLocaleDateString(),
+    new Date("2027-07-01").toLocaleDateString(),
+  ];
+  const revisionDates = calculateRevisionDate(selectedDate);
+  assert.equal(revisionDates,expectedDates);
 });
