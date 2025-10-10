59ce65ac907979dc6e2785b5284ebaf701925dc7
sheida-shab <shabankary@gmail.com>
2025-10-10 18:22:56 +0100

    Fixing final issue about showing today in user agenda and remove some comments on the top of file

--- script.mjs
+++ script.mjs
@@ -1,9 +1,3 @@
-// This is a placeholder file which shows how you can access functions defined in other files.
-// It can be loaded into index.html.
-// You can delete the contents of the file once you have understood how it works.
-// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
-// You can't open the index.html file using a file:// URL.
-
 
 import { getUserIds } from "./common.mjs";
 import { getData } from "./storage.mjs";
@@ -175,10 +169,12 @@ window.onload = function () {
     const date = new Date(dateValue);
     const revisionDates = calculateRevisionDate(date);
     const today = new Date();
+    today.setHours(0, 0, 0, 0);
 
     const agendaItems = revisionDates
       .map((dateStr) => {
         const revDate = new Date(dateStr);
+        revDate.setHours(0, 0, 0, 0);
         return revDate >= today ? { topic, date: dateStr } : null;
       })
       .filter(Boolean);
